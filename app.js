dc6f0599db6cf8e4d15776b93e26009447710eaa
TarStaBor <TarStaBor@yandex.ru>
2021-11-02 19:09:20 +0300

    fix: fix CORS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const {
 } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
